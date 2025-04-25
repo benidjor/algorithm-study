4862a7aa63b68259d5bc932d7b9ad8b26d65ab5e
전상택 (Sangteck Jeon) <94089198+benidjor@users.noreply.github.com>
2025-04-25 23:41:53 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+import sys
+
+score = int(sys.stdin.readline().strip())
+
+if (score >= 90) & (score <= 100):
+    print("A")
+elif (score >= 80) & (score <=89):
+    print("B")
+elif (score >= 70) & (score <= 79):
+    print("C")
+elif (score >= 60) & (score <= 69):
+    print("D")
+else:
+    print("F")

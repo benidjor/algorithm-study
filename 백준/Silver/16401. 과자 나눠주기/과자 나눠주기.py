6881dbfc46b124839e13b94a69391391e3bac4bd
전상택 (Sangteck Jeon) <94089198+benidjor@users.noreply.github.com>
2025-10-26 17:27:28 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1652 ms, Memory: 146176 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,49 @@
+import sys
+
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+
+cookies = list(map(int, input().split()))
+
+# 과자를 잘랐을 때, 몇 조각이 나오는지 계산
+def cutting(length, cookies):
+    # 총 과자 조각 수
+    slices = 0
+    # 과자 배열에서 각각의 과자 길이를 가져와서 설정한 길이로 나눈다
+    for cookie in cookies:
+        # 과자를 특정 길이로 나누었을 때, 몇 조각이 나오는지 더해준다
+        slices += (cookie // length)
+    
+    return slices
+
+# print(cutting(7, cookies))
+
+def parametic_binary_search(array, target, start, end):
+    # 조카들에게 나눠줄 과자가 없다면 0을 출력
+    result = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+        # mid 길이로 과자를 잘랐을 때, 몇 조각이 나오는지 계산
+        slices = cutting(mid, array)
+        
+        # 만약 mid 길이가 너무 커서 target 개수보다 작게 잘렸다
+        if slices < target:
+            # mid 길이를 줄여야 한다 (왼쪽 탐색) 
+            end = mid - 1
+        else:
+            result = mid
+            start = mid + 1
+    
+    return result
+
+
+start = 1
+end = max(cookies)
+
+print(parametic_binary_search(cookies, M, start, end))
+
+
+
+

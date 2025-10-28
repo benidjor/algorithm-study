466fe9d3bdd0907836337d910632084db10ea12f
전상택 (Sangteck Jeon) <94089198+benidjor@users.noreply.github.com>
2025-10-28 20:56:32 +0900

    [Silver I] Title: 스타트와 링크, Time: 1964 ms, Memory: 57548 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,58 +1,46 @@
 import sys
 from itertools import combinations
-from collections import defaultdict
 
 input = sys.stdin.readline
 
 N = int(input())
-
-# 입력받는 능력치 2차원 배열
 arr = [list(map(int, input().split())) for _ in range(N)]
+
+# 스타트, 링크 팀 차집합 계산 위해 set 설정
+players = set(range(N))
 
 # 팀원들로 생성할 수 있는 팀 조합 생성
 team_combis_list = list(combinations(range(N), N//2))
 
-# 팀원들의 능력치 조회를 위한 조합 생성
-combinations_list = list(combinations(range(N), 2))
-
-# 팀 조합을 위해 defaultdict 사용
-team_dict = defaultdict(list)
-
-for team_combi in team_combis_list:
-    # 생성된 팀원 조합에 없는 사람들은 반대 팀에 배정
-    opposit_list = [item for item in list(range(N)) if item not in team_combi]
-
-    # team[홈 팀 팀원] = 어웨이 팀 팀원
-    team_dict[team_combi] = opposit_list
-
-for key, value in team_dict.items():
-
-    # 홈, 어웨이 팀원들의 능력치를 알아보기 위해 조합 생성
-    key_combis = list(combinations(key, 2))
-    value_combis = list(combinations(value, 2))
-
-    key_attribute = 0
-    value_attribute = 0
-    
-    # 홈팀 팀원들의 능력치 계산
-    for x, y in key_combis:
-        key_attribute += arr[y][x] + arr[x][y]
-        # 홈팀 팀원들의 능력치 계산하여 value로 지정
-        team_dict[key] = [key_attribute]
-    
-    # 어웨이 팀원들의 능력치 계산
-    for x, y in value_combis:
-        value_attribute += arr[y][x] + arr[x][y]
-    # 어웨이팀 팀원들의 능력치 계산하여 이미 value로 지정되어 잇는 홈팀 능력치 리스트에 요소 추가
-    team_dict[key].append(value_attribute)
-    
 result = float("inf")
 
-# 홈팀과 어웨이팀 전력차를 계산
-for value in team_dict.values():
-    diff = abs(value[0] - value[1])
+list_len = len(team_combis_list)
+
+# 스타트, 링크 팀이 바뀌어도 능력치 차이는 동일하기 때문에, 중복 계산 방지를 위해 절반만 순회
+for i in range(list_len//2):
     
+    start_team = team_combis_list[i]
+
+    link_team = list(players - set(start_team))
+
+    start_attribute = 0
+    link_attribute = 0
+
+    # 스타트, 링크 팀의 능력치를 즉시 계산
+    for x, y in combinations(start_team, 2):
+        start_attribute += arr[y][x] + arr[x][y]
+    
+    for x, y in combinations(link_team, 2):
+        link_attribute += arr[y][x] + arr[x][y]
+    
+    # 즉시 최소값 갱신
+    diff = abs(start_attribute - link_attribute)
+
     if diff < result:
         result = diff
+    
+    # 만약 스타트, 링크 팀 능력치 차이가 0이라면, 더 이상 탐색할 필요가 없다
+    if result == 0:
+        break
 
 print(result)

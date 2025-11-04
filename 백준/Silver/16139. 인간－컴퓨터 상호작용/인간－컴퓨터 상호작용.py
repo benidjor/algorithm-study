acdbffff2ee8b89fa36ccd7665faf554ec329069
전상택 (Sangteck Jeon) <94089198+benidjor@users.noreply.github.com>
2025-11-05 01:08:21 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 256 ms, Memory: 169036 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -2,26 +2,44 @@ import sys
 
 input = sys.stdin.readline
 
-string = input()
+strings = input().strip()
 q = int(input())
+length = len(strings)
 
-def prefix_sum(arr, length):
-    prefix_sum_list = [0] * (length+1)
-    for i in range(1, length+1):
-        prefix_sum_list[i] = prefix_sum_list[i-1] + arr[i-1]
+result = []
+
+# 0번 인덱스 0으로 더미 패딩
+alphabet_count = [[0] * (length+1) for _ in range(26)]
+
+# 2차원 누적합 배열 계산
+def prefix_sum_2d_alphabet(arr, length):
     
-    return prefix_sum_list
+    # 모든 알파벳을 인덱스로 삼는다 (a ~ z)
+    for i in range(26):
+        # 문자열의 1부터 length + 1 번째 위치까지 순회
+        for j in range(1, length+1):
+            
+            # 이전 위치의 알파벳 누적합 값 그대로 복사
+            arr[i][j] = arr[i][j-1]
 
+            # 문자열의 현재 알파벳에 해당하는 위치가 순회중인 알파벳 인덱스와 동일하다면 누적합에 1 더한다
+            current_string_idx = ord(strings[j-1]) - 97
+
+            if current_string_idx == i:
+                arr[i][j] += 1
+    
+    return arr
+
+prefix_sum = prefix_sum_2d_alphabet(alphabet_count, length)
 
 for _ in range(q):
     a, l, r = input().split()
+    a_idx = ord(a) - 97
     l = int(l)
     r = int(r)
-    count_alphabet = [1 if char == a else 0 for char in string]
 
-    prefix_sum_list = prefix_sum(count_alphabet, len(string))
-    # print(prefix_sum_list)
-    print(prefix_sum_list[r+1] - prefix_sum_list[l])
+    cnt = prefix_sum[a_idx][r+1] - prefix_sum[a_idx][l]
 
+    result.append(str(cnt))
 
-
+print("\n".join(result))

fdb73dd75548ab5ef0f428fbf82424f664ea0ac3
전상택 (Sangteck Jeon) <94089198+benidjor@users.noreply.github.com>
2025-11-06 03:08:01 +0900

    [Silver I] Title: 경로 찾기, Time: 348 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -5,29 +5,22 @@ input = sys.stdin.readline
 N = int(input())
 INF = float("inf")
 
-# # 2차원 리스트 (그래프 표현) 만들고, 무한으로 초기화
-# graph = [[INF] * (N+1) for _ in range(N+1)]
 
-# # 자기 자신에게서 자기 자신으로 가는 비용 0으로 초기화
-# for a in range(1, N+1):
-#     for b in range(1, N+1):
-#         if a == b:
-#             graph[a][b] = 0
-
+# 원본 그래프 input으로 입력
 graph = [list(map(int, input().split())) for _ in range(N)]
 
+# 0은 아직 직접적으로 연결되었는지 모르는 경로이기 때문에, INF로 변경
 graph_inf = [[INF if value == 0 else value for value in row] for row in graph]
-# print(graph_inf)
 
 
+# 플로이드 워셜 알고리즘 통해 최단 경로 꼐산
 for k in range(N):
     for a in range(N):
         for b in range(N):
+            # k를 거치는 경로가 더 짧다면 갱신
             graph_inf[a][b] = min(graph_inf[a][b], graph_inf[a][k] + graph_inf[k][b])
 
-# print(graph_inf)
-
-
+# 최종 거리가 INF라면 경로가 없다는 뜻이고, INF가 아닌 숫자라면 경로가 있다는 의미이다
 result = [[0 if value == INF else 1 for value in row] for row in graph_inf]
 
 for row in result:

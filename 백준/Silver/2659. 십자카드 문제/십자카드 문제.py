bb6a121105290eecf93c24729a7397dc3816a0af
전상택 (Sangteck Jeon) <94089198+benidjor@users.noreply.github.com>
2025-11-18 20:48:12 +0900

    [Silver III] Title: 십자카드 문제, Time: 132 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+
+input_num_list = list(map(int, input().split()))
+
+def making_clock_num(num_list):
+    
+    clock_num_list = []
+
+    for i in range(len(num_list)):
+
+        modified_list = num_list[i:] + num_list[:i]
+        
+        possible_clock_num = int("".join(map(str, modified_list)))
+        clock_num_list.append(possible_clock_num)
+
+    return min(clock_num_list)
+
+clock_num = making_clock_num(input_num_list)
+
+cnt = 0
+checked = []
+
+for num in range(1111, 10000):
+
+    if 0 in list(map(int, str(num))):
+        continue
+
+    num = making_clock_num(list(map(int, str(num))))
+
+    if num in checked:
+        continue
+    
+    cnt += 1
+    if num == clock_num:    
+        break
+
+    checked.append(num)
+    
+
+print(cnt)

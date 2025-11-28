666df259caadfd39dc309a1f40caa2d5dd460c6c
전상택 (Sangteck Jeon) <94089198+benidjor@users.noreply.github.com>
2025-11-28 11:43:25 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+"""
+max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i]
+"""
+
+stairs = [0] + [int(input()) for _ in range(N)]
+
+if N == 1:
+    print(stairs[1])
+
+else:
+    dp = [0] * (N+1)
+
+    dp[1] = stairs[1]
+    dp[2] = dp[1] + stairs[2]
+
+    for i in range(3, N+1):
+        dp[i] = max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i]
+    
+    print(dp[N])

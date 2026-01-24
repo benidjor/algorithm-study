bcf6a232d33520ffdb537c8cfa4ff85a3b791af8
전상택 (Sangteck Jeon) <benidjor@gmail.com>
2026-01-24 09:21:08 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,49 @@
+# SELECT *
+# FROM ITEM_INFO AS I
+# INNER JOIN ITEM_TREE AS T
+#     ON I.ITEM_ID = T.ITEM_ID
+
+
+SELECT I.ITEM_ID, I.ITEM_NAME, I.RARITY
+FROM ITEM_INFO AS I
+WHERE NOT EXISTS (
+    SELECT T.PARENT_ITEM_ID 
+    FROM ITEM_TREE AS T 
+    WHERE I.ITEM_ID = T.PARENT_ITEM_ID
+)
+ORDER BY I.ITEM_ID DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT I.ITEM_ID, I.ITEM_NAME, I.RARITY
+# FROM ITEM_INFO AS I
+# -- 더 이상 업그레이드할 수 없는 아이템 : PARENT_ITEM_ID에 없는 ITEM_ID를 찾는다
+# WHERE NOT EXISTS (
+#                     SELECT *
+#                     FROM ITEM_TREE AS T
+#                     WHERE I.ITEM_ID = T.PARENT_ITEM_ID
+# )
+# ORDER BY I.ITEM_ID DESC
